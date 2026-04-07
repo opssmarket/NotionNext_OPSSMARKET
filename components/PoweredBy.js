8fd4e3be6f8addb8de158468590c4cc7bdcd9333
opssmarket <info@opssmarket.com>
2026-04-07 20:54:22 +0200

    Update link and text in PoweredBy component

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -8,8 +8,8 @@ export default function PoweredBy(props) {
   return (
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
       <span className='mr-1'>Copyright ©</span>
-      <a href='你的链接' className='underline justify-start'>
-        你的名字或站点名
+      <a href='https://opssmarket.com' className='underline justify-start'>
+        OPSS MARKET
       </a>
     </div>
   )
