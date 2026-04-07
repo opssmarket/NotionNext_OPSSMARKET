b070bf66afa3f40db37c2782c3c5a9e985d279a9
opssmarket <info@opssmarket.com>
2026-04-07 20:53:26 +0200

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -7,13 +7,12 @@ import { siteConfig } from '@/lib/config'
 export default function PoweredBy(props) {
   return (
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
-      <span className='mr-1'>Powered by</span>
-      <a
-        href='https://github.com/tangly1024/NotionNext'
-        className='underline justify-start'>
-        NotionNext {siteConfig('VERSION')}
+      <span className='mr-1'>Copyright ©</span>
+      <a href='你的链接' className='underline justify-start'>
+        你的名字或站点名
       </a>
-      .
     </div>
   )
 }
+
+
